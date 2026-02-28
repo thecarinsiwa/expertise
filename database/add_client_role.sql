a8481b79f29633115493568501b72c48d0ebef2d
thecarinsiwa <thecarinsiwa@gmail.com>
2026-02-28 17:38:02 +0200

    Update schema.sql file

--- database/add_client_role.sql
+++
@@ -1,4 +0,0 @@
--- Rôle système Client pour l'espace client (inscription publique, tableau de bord)
--- Organisation 1 = Expertise Humanitaire et Sociale SARL (défaut)
-INSERT IGNORE INTO `role` (`id`, `organisation_id`, `name`, `code`, `is_system`)
-VALUES (5, 1, 'Client', 'client', 1);
